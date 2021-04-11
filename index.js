d71a54b42a9a35db002f8144acc44e4dbf840b1c
Duy Pham Le <phamleduy04@gmail.com>
2021-04-11 07:58:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.commands = new Collection();
 client.aliases = new Collection();
 client.catagories = readdirSync('./commands/');
 client.instance = axios.create({
-    baseURL: 'https://localhost:3000',
+    baseURL: 'http://localhost:3000',
 });
 ["command"].forEach(handler => {
     require(`./handlers/${handler}`)(client);
